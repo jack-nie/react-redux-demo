52ad9447cff7011cd7468608b2d91c729b0855f6
jack-nie <hust.ud.hui@gmail.com>
2019-02-18 15:02:48 +0800

    upgrade react and many modules

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var TARGET_PATH = path.join(__dirname, 'assets');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://127.0.0.1:8080', 
+    'webpack-dev-server/client?http://127.0.0.1:8080',
     'webpack/hot/only-dev-server',
     path.join(SRC_PATH, 'js', 'entry.js')
   ],
@@ -15,23 +15,23 @@ module.exports = {
     filename: '[name].js',
     publicPath: ''
   },
+  mode: "development",
   module: {
-    loaders: [
-      { test: /\.css$/, exclude: /node_modules/, loader: "style-loader!css-loader"  },
-      { test: /\.png$/, exclude: /node_modules/, loader: "url-loader?limit=100000"  },
-      { test: /\.jpg$/, exclude: /node_modules/, loader: "file-loader"  },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    rules: [
+      { test: /\.css$/, exclude: /node_modules/, use: "style-loader!css-loader"  },
+      { test: /\.png$/, exclude: /node_modules/, use: "url-loader?limit=100000"  },
+      { test: /\.jpg$/, exclude: /node_modules/, use: "file-loader"  },
+      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new htmlWebPackPlugin({
       inject: true,
       template: 'src/index.html'
-    }),
-    new webpack.NoErrorsPlugin()
+    })
   ]
 };
